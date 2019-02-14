bb4e23826337fd6d5dc8934d2b54476f2f15909d
刘欢 <331319341@qq.com>
2019-02-14 12:09:16 +0800

    序列化1,django自带方法

--- tushu/APP01/views.py
+++ tushu/APP01/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from app01 import  models
+
+from APP01 import  models
 from django.http import HttpResponse
 def 出版社列表(requset):
     结果=models.出版社.objects.all()
-
+    # 方法1
     # data=[]
     # for i in 结果:
     #     p_tpm={
@@ -11,10 +12,15 @@ def 出版社列表(requset):
     #          "地址": i.地址 }
     #     data.append(p_tpm)
 
-    data=[]
-    from django.forms.models import model_to_dict
-    for i in 结果:
-        data.append(model_to_dict(i))
+    #方法2
+    # data=[]
+    # from django.forms.models import model_to_dict
+    # for i in 结果:
+    #     data.append(model_to_dict(i))
+
+    # 方法3
+    from django.core import serializers
+    data=serializers.serialize("json",结果)
+
     import json
     return HttpResponse(json.dumps(data),content_type='application/json')
-# Create your views here.
