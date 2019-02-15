f1cf8a69033d31d99538e47c22dd5b8b72b68de0
刘欢 <331319341@qq.com>
2019-02-15 17:12:18 +0800

    rest  序列化,不用写模型 (简化序列化)

--- tushu/APP01/序列化.py
+++ tushu/APP01/序列化.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from APP01 import models
-class 出版社序列化(serializers.Serializer):
-    id= serializers.IntegerField(read_only=True)
-    名称=serializers.CharField(max_length=32)
-    地址=serializers.CharField(max_length=32)
+# class 出版社序列化(serializers.Serializer):
+#     id= serializers.IntegerField(read_only=True)
+#     名称=serializers.CharField(max_length=32)
+#     地址=serializers.CharField(max_length=32)
+#
+#     def create(self, validated_data):
+#         return  models.出版社.objects.create(**validated_data)
+#     def update(self, instance, validated_data):
+#         instance.名称=validated_data("名称",instance)
+#         instance.地址=validated_data("地址",instance)
+#         instance.save()
+#         return instance
 
-    def create(self, validated_data):
-        return  models.出版社.objects.create(**validated_data)
-    def update(self, instance, validated_data):
-        instance.名称=validated_data("名称",instance)
-        instance.地址=validated_data("地址",instance)
-        instance.save()
-        return instance
 # from APP01 import models
 # from APP01 import models ,序列化
 # p1=models.出版社.objects.first()
@@ -22,3 +23,8 @@ class 出版社序列化(serializers.Serializer):
 # s.is_valid()
 # s.validated_data
 # s.save()
+
+class 出版社序列化(serializers.ModelSerializer):
+    class Meta:
+        model=models.出版社
+        fields = ('id', '名称', '地址')
